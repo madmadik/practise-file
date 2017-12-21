78487d326a8c0d273279a6f3ae4ef83e9802f611
madik <madik404error@gmail.com>
2017-12-21 20:22:34 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Task1()
+        {
+            Dictionary<char, int> all = new Dictionary<char, int>();
+            char[] mas;
+            using (FileStream stream = new FileStream(@"C:\Users\madik\Documents\Visual Studio 2017\projects\ConsoleApp19\test.txt", FileMode.Open))
+            {
+                byte[] bytes = new byte[stream.Length];
+                stream.Read(bytes, 0, bytes.Length);
+                mas = Encoding.UTF8.GetChars(bytes);
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    if (!all.ContainsKey(mas[i]))
+                    {
+                        all.Add(mas[i], 1);
+                    }
+                    else
+                    {
+                        all[mas[i]]++;
+                    }
+                }
+            }
+            foreach (KeyValuePair<char, int> value in all)
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+        static void Task2()
+        {
+            using (StreamWriter stream = new StreamWriter(@"C:\Users\madik\Documents\Visual Studio 2017\projects\ConsoleApp19\test2.txt"))
+            {
+                stream.WriteLine("mad");
+                stream.WriteLine("madik");
+                stream.WriteLine("404error");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Console.ReadLine();
+        }
+    }
+}
